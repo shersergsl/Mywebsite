f181dd8b5753251cca596415bf538498ef2faa47
shersergsl <89525388+shersergsl@users.noreply.github.com>
2021-11-29 09:41:56 +0200

    accumulated changes

---
+++ Mywebsite/js/sсript.js
@@ -0,0 +1,59 @@
+const colo_l = document.querySelector('.colo_l');
+const centr_l = document.querySelector('.centr_l'); 
+const leftblock_r = document.querySelector('.leftblock_r');
+const leftblock_l = document.querySelector('.leftblock_l');
+const colo_r = document.querySelector('.colo_r');
+const centr_r = document.querySelector('.centr_r');
+const rightblock_r = document.querySelector('.rightblock_r');
+const rightblock_l = document.querySelector('.rightblock_l');
+
+colo_l.addEventListener('mouseenter', function (createStyle)
+{
+    centr_l.style.backgroundColor = 'rgb(0, 0, 0,0)';
+    centr_l.style.width = '106px';
+    centr_l.style.height ='62px';
+    centr_l.style.backgroundImage = 'radial-gradient(circle at 53px -20px, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0) 60px, rgb(5,55,105) 61px)';
+    centr_l.style.color = '#FFF';
+    leftblock_r.style.borderTopLeftRadius ='40px 25px';
+    leftblock_r.style.margin ='0px -2px';
+    leftblock_l.style.borderTopRightRadius ='40px 25px';
+    leftblock_l.style.margin ='0px -2px';
+});
+
+colo_l.addEventListener('mouseleave', function (createStyle)
+{
+    centr_l.style.backgroundColor = 'rgb(5, 55,105)';
+    centr_l.style.width = '104px';
+    centr_l.style.height ='70px';
+    centr_l.style.backgroundImage = 'radial-gradient(circle at 53px -20px, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0) 60px, rgb(5,55,105) 61px)';
+    centr_l.style.color = '#FFF';
+    leftblock_r.style.borderTopLeftRadius ='0px';
+    leftblock_r.style.margin ='0px -2px';
+    leftblock_l.style.borderTopRightRadius ='0';
+    leftblock_l.style.margin ='0px 0px';
+});
+colo_r.addEventListener('mouseenter', function (createStyle)
+{
+    centr_r.style.backgroundColor = 'rgb(0, 0, 0,0)';
+    centr_r.style.width = '106px';
+    centr_r.style.height ='62px';
+    centr_r.style.backgroundImage = 'radial-gradient(circle at 53px -20px, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0) 60px, rgb(5,55,105) 61px)';
+    centr_r.style.color = '#FFF';
+    rightblock_r.style.borderTopLeftRadius ='40px 25px';
+    rightblock_r.style.margin ='0px -2px';
+    rightblock_l.style.borderTopRightRadius ='40px 25px';
+    rightblock_l.style.margin ='0px -2px';
+});
+
+colo_r.addEventListener('mouseleave', function (createStyle)
+{
+    centr_r.style.backgroundColor = 'rgb(5, 55,105)';
+    centr_r.style.width = '104px';
+    centr_r.style.height ='70px';
+    centr_r.style.backgroundImage = 'radial-gradient(circle at 53px -20px, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0) 60px, rgb(5,55,105) 61px)';
+    centr_r.style.color = '#FFF';
+    rightblock_r.style.borderTopLeftRadius ='0px';
+    rightblock_r.style.margin ='0px -2px';
+    rightblock_l.style.borderTopRightRadius ='0';
+    rightblock_l.style.margin ='0px 0px';
+});
---
+++ testJS/js/skript.js
@@ -0,0 +1,14 @@
+//const kub3 = document.querySelector('.kub3');
+//const kub1 = document.querySelector('.kub:hover');
+//const kub2 = document.querySelector('.kub2');
+
+//kub2.addEventListener('mouseover', function (createStyle)
+//{
+  
+    //kub3.style.borderRadius = '50px';
+//});
+//kub2.addEventListener('mouseout', function (createStyle)
+//{
+
+   // kub3.style.borderRadius = '0px';
+//});
